60d068f77dfa68151f6e5f37d95e57f12c8d7c16
wangzhongxin <940945444@qq.com>
2017-04-22 20:44:44 +0800

    third commit

---
+++ Assets/HelloAR/Scripts/BtnClick.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnClick : MonoBehaviour {
+
+	public float speed=100.0f;
+	private GameObject gameObj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (gameObj != null) {
+			
+			
+			//gameObj.transform.Rotate(Vector3.up*Time.deltaTime*speed);
+			gameObj.transform.Rotate(Vector3.up*Time.deltaTime*speed);
+			
+		}
+	
+	}
+
+	public void btnClick(){
+
+
+		gameObj = GameObject.Find ("first");
+
+
+
+
+
+	}
+}
